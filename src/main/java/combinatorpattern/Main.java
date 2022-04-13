3d4157f15adc688d5aa77c8289b0c7c5a9e2b751
Krystian <krystian14d@gmail.com>
2022-04-13 08:49:07 +0200

    combinator pattern

--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -2,16 +2,30 @@ package combinatorpattern;
 
 import java.time.LocalDate;
 
+import static combinatorpattern.CustomerRegistrationValidator.*;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
                 "Alice",
-                "alice@gmail.com",
+                "alicegmail.com",
                 "+2345463243",
                 LocalDate.of(2000, 1, 1)
         );
 
-        CustomerValidatorService validatorService = new CustomerValidatorService();
-        System.out.println(validatorService.isValid(customer));
+//        CustomerValidatorService validatorService = new CustomerValidatorService();
+//        System.out.println(validatorService.isValid(customer));
+
+        //using combinator pattern
+        ValidationResult result = isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if(result != ValidationResult.SUCCESS){
+            throw new IllegalStateException(result.name());
+        }
     }
 }
